5ce6525f221e83e6071e6b1e6283dc5670e61907
vaneshita <144995@unsaac.edu.pe>
2017-11-21 07:30:33 -0500

    replicando menu drop down de lms de casos practicos

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+var submenu = document.getElementsByClassName('submenu');
+for (var i = 0; i < submenu.length; i++) {
+  submenu[i].addEventListener('click', showMenu);
+}
+function showMenu() {
+  var listMenu = this.getElementsByClassName('itemList')[0];
+  console.log(listMenu);
+  if (listMenu.classList.contains('hide')) {
+    listMenu.classList.remove('hide');
+    listMenu.classList.add('show');
+  } else {
+    listMenu.classList.remove('show');
+    listMenu.classList.add('hide');
+  }
+}
